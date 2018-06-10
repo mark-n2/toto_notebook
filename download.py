7737aec8abd50fd31a589bf97bf5dc319e0297be
Masashi Suguro <ecologydriver@gmail.com>
2018-06-10 21:57:54 +0900

    #1 公式記録をダウンロードする部分を作成

--- download.py
+++ download.py
@@ -1,22 +1,38 @@
-import requests
-from html.parser import HTMLParser
+import pandas as pd
+import sqlite3
+import os
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print('Encountered a start tag:', tag)
-    def handle_endtag(self, tag):
-        print('Encountered an end tag :', tag)
-    def handle_data(self, data):
-        print('Encountered some data :', data)
+DB_FOLDER = "data"
+DB_NAME = "jleague.db"
 
-year = 2018
-competition = 1
-
-url = 'https://data.j-league.or.jp/SFMS01/search?competition_years='+ str(year) + '&competition_frame_ids=' + str(competition)
-
-print(url)
-res = requests.get(url)
-res.raise_for_status()
-
-parser = MyHTMLParser()
-parser.feed(res.text)
+def download_result(year, competition=0):
+    """
+    Jリーグの公式記録ページから、勝敗記録を取得する。
+    
+    API等は無いのでHTMLから取得するが、必要データはtableタグのみであり、pandasで取得
+    戻り値はDataFrame型
+    """
+    if competition == 0:
+        url = 'https://data.j-league.or.jp/SFMS01/search?competition_years='+ str(year)
+    else:
+        url = 'https://data.j-league.or.jp/SFMS01/search?competition_years='+ str(year) + '&competition_frame_ids=' + str(competition)
+    dfs = pd.io.html.read_html(url)
+    # 取得したDataFrameの一番目が結果表
+    df = dfs[0].drop(['インターネット中継・TV放送','入場者数'],axis=1) # 結果に関係なさそうな放送局と入場者数はこの時点で削除しておく
+    return dfs[0]
+    
+def download_all_results():
+    """
+    Jリーグの公式記録ページから、勝敗記録を取得し、SQLite3データベース化する。
+    1992年～2017年のデータを取得
+    """
+    result = pd.DataFrame()
+    for year in range(1992,2018):
+        df = download_result(year)
+        result = pd.concat([result, df])
+    
+    db_name = DB_FOLDER + '/' + DB_NAME
+    if os.path.exists(DB_FOLDER) == False:
+        os.mkdir(DB_FOLDER)
+    conn = sqlite3.connect(db_name) # DB接続
+    result.to_sql("result", conn, if_exists='replace') # DFをDBに変換
